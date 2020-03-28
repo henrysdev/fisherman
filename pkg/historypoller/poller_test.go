1579d4fd7b7b1b1fb6fd79d4e685efbe482f5fea
henry.warren <henry.warren@compass.com>
2020-03-28 16:02:47 -0400

    [1] Poll for Positive Diffs WIP
    - Started impl for positive diffs
    - Cleaned up utils unit tests
    - Renamed package `poller` -> `historypoller`

--- pkg/historypoller/poller_test.go
+++ pkg/historypoller/poller_test.go
@@ -1,4 +1,4 @@
-package poller
+package historypoller
 
 import (
 	"os"
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -9,17 +9,15 @@ import (
 )
 
 const (
-	filename = "testfile"
+	filename  = "testfile"
+	filename1 = filename + "_1"
 )
 
 func genArbitraryFile(name string, size int) {
 	// Ex: dd if=/dev/zero of=output.dat bs=24M count=1
-	cmd := exec.Command(
-		"dd",
-		[]string{"if=/dev/zero",
-			fmt.Sprintf("of=%s", name),
-			fmt.Sprintf("bs=%v", size),
-			"count=1"}...)
+	nameClause := fmt.Sprintf("of=%s", name)
+	sizeClause := fmt.Sprintf("bs=%v", size)
+	cmd := exec.Command("dd", "if=/dev/zero", nameClause, sizeClause, "count=1")
 	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
@@ -27,26 +25,42 @@ func genArbitraryFile(name string, size int) {
 }
 
 func TestFileExists_WhenDoesExist_True(t *testing.T) {
+	// Arrange
 	os.Create(filename)
-	if !FileExists(filename) {
+	defer os.Remove(filename)
+
+	// Act
+	fileExists := FileExists(filename)
+
+	// Assert
+	if !fileExists {
 		t.Errorf("File `%v` should exist", filename)
 	}
-	os.Remove(filename)
 }
 
 func TestFileExists_WhenDoesNotExist_False(t *testing.T) {
+	// Arrange
 	os.Create(filename)
 	os.Remove(filename)
-	if FileExists(filename) {
+
+	// Act
+	fileExists := FileExists(filename)
+
+	// Assert
+	if fileExists {
 		t.Errorf("File `%v` shoud not exist", filename)
 	}
 }
 
 func TestOpenFileAtPos_WhenValidFileAndSeek_NoError(t *testing.T) {
+	// Arrange
 	genArbitraryFile(filename, 100)
 	defer os.Remove(filename)
 
+	// Act
 	file, err := OpenFileAtPos(filename, 60)
+
+	// Assert
 	if err != nil {
 		t.Error("Failed to open file at position")
 	}
@@ -58,7 +72,10 @@ func TestOpenFileAtPos_WhenValidFileAndSeek_NoError(t *testing.T) {
 }
 
 func TestOpenFileAtPos_WhenInvalidFile_Error(t *testing.T) {
+	// Act
 	_, err := OpenFileAtPos(filename, 60)
+
+	// Assert
 	if err == nil {
 		t.Error("Failed to error on opening invalid file")
 	}
@@ -68,10 +85,14 @@ func TestOpenFileAtPos_WhenInvalidFile_Error(t *testing.T) {
 }
 
 func TestOpenFileAtPos_WhenOverflowSeek_Error(t *testing.T) {
+	// Arrange
 	genArbitraryFile(filename, 100)
 	defer os.Remove(filename)
 
+	// Act
 	_, err := OpenFileAtPos(filename, 300)
+
+	// Assert
 	if err == nil {
 		t.Error("Failed to error on seeking to invalid position")
 	}
@@ -81,10 +102,14 @@ func TestOpenFileAtPos_WhenOverflowSeek_Error(t *testing.T) {
 }
 
 func TestOpenFileAtPos_WhenInvalidSeek_Error(t *testing.T) {
+	// Arrange
 	genArbitraryFile(filename, 100)
 	defer os.Remove(filename)
 
+	// Act
 	_, err := OpenFileAtPos(filename, -1)
+
+	// Assert
 	if err == nil {
 		t.Error("Failed to error on seeking to invalid position")
 	}
@@ -94,17 +119,20 @@ func TestOpenFileAtPos_WhenInvalidSeek_Error(t *testing.T) {
 }
 
 func TestReadRestOfFile_WhenValid_NoError(t *testing.T) {
-	// Write some file contents
+	// Arrange
 	arbFile, _ := os.Create(filename)
+	defer os.Remove(filename)
 	fileContents := []byte("ABCDEF")
 	arbFile.Write(fileContents)
 	arbFile.Close()
-
-	// Seek to an arbitrary position and assert that rest read is correct
 	testFile, _ := os.Open(filename)
 	seekPos := int64(2)
 	testFile.Seek(seekPos, 0)
+
+	// Act
 	rest, err := ReadRestOfFile(testFile, seekPos)
+
+	// Assert
 	if err != nil {
 		t.Errorf("Failed to read rest of file without error: %v", err)
 	}
@@ -113,19 +141,68 @@ func TestReadRestOfFile_WhenValid_NoError(t *testing.T) {
 			t.Errorf("Rest data mismatch %v vs %v", fileContents[int(seekPos)+i], b)
 		}
 	}
-	os.Remove(filename)
 }
 
 func TestReadRestOfFile_WhenInvalidFile_Error(t *testing.T) {
+	// Act
 	_, err := ReadRestOfFile(nil, 100)
+
+	// Assert
 	if err == nil {
 		t.Error("Failed to error on invalid file")
 	}
 }
 
 func TestReadRestOfFile_WhenInvalidSeek_Error(t *testing.T) {
+	// Act
 	_, err := ReadRestOfFile(nil, -100)
+
+	// Assert
 	if err == nil {
 		t.Error("Failed to error on invalid seek position")
 	}
+}
+
+func TestFilesDiffer_WhenNotDifferent_False(t *testing.T) {
+	// Arrange
+	genArbitraryFile(filename, 100)
+	cmd := exec.Command("cp", filename, filename1)
+	cmd.Output()
+	defer os.Remove(filename)
+	defer os.Remove(filename1)
+
+	// Act
+	res, err := FilesDiffer(filename, filename1)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Failed by throwing error. No error should be thrown. Error: %v", err)
+	}
+	if res {
+		t.Error("Failed. Should be no diff")
+	}
+}
+
+func TestFilesDiffer_WhenDifferent_True(t *testing.T) {
+	// Arrange
+	file, _ := os.Create(filename)
+	file1, _ := os.Create(filename1)
+	defer os.Remove(filename)
+	defer os.Remove(filename1)
+	file.Write([]byte("abd"))
+	file1.Write([]byte("ipoj"))
+	file.Close()
+	file1.Close()
+
+	// Act
+	res, err := FilesDiffer(filename, filename1)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Failed by throwing error %v", err)
+	}
+	if !res {
+		t.Error("Failed by returning no diff")
+	}
+
 }

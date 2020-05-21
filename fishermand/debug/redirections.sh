4fdc699bf03ea36afbbf6235a8b5028068d615d6
Henry Warren <henrysdev@gmail.com>
2020-05-20 20:41:21 -0400

    Delete redirections.sh

--- fishermand/debug/redirections.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Write stdout and stderr to log files
-COMMAND_HERE > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
-
-# Write just stderr to log file
-COMMAND_HERE 2> >(tee -a stderr.log >&2)

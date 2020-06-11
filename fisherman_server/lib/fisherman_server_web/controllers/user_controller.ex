bc70fa84ca0d391a6257150ada2171ca0d51e647
henry.warren <henry.warren@compass.com>
2020-06-10 20:10:54 -0400

    Directory Structure Refactor
    - Refactored dirs
    - Simplified User controller

--- fisherman_server/lib/fisherman_server_web/controllers/user_controller.ex
+++ fisherman_server/lib/fisherman_server_web/controllers/user_controller.ex
@@ -13,28 +13,13 @@ defmodule FishermanServerWeb.UserController do
   Creates and inserts a new user object
   """
   def create(conn, params) do
-    user = handle_user(params)
+    user =
+      %User{}
+      |> User.changeset(params)
+      |> Repo.insert!()
+      |> unmarshal()
+
     json(conn, user)
-  end
-
-  defp handle_user(user) do
-    {:ok, user} =
-      user
-      |> marshal()
-      |> Repo.insert()
-
-    unmarshal(user)
-  end
-
-  # TODO can this be accomplished with a schemaless changeset?
-  defp marshal(user = %{}) do
-    %User{
-      username: get_in(user, ["username"]),
-      email: get_in(user, ["email"]),
-      machine_serial: get_in(user, ["machine_serial"]),
-      first_name: get_in(user, ["first_name"]),
-      last_name: get_in(user, ["last_name"])
-    }
   end
 
   defp unmarshal(user = %User{}) do

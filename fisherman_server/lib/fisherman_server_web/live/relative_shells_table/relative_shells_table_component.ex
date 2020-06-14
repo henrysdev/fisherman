6ab135ee64fb0625e298384568093d0accca4b56
henry.warren <henry.warren@compass.com>
2020-06-14 11:25:11 -0400

    Refactor modules + cleanup live view component

--- fisherman_server/lib/fisherman_server_web/live/relative_shells_table/relative_shells_table_component.ex
+++ fisherman_server/lib/fisherman_server_web/live/relative_shells_table/relative_shells_table_component.ex
@@ -6,21 +6,26 @@ defmodule FishermanServerWeb.Live.RelativeShellsTable.RelativeShellsTableCompone
 
   def render(assigns) do
     ~L"""
-      <table border="1" id="relative-shells-table">
+      <style>
+        .tcol {
+          width: <%= 100 / max(1, length(@pids)) %>%;
+        }
+      </style>
+
+      <table width="100%" border="1" id="relative-shells-table">
 
         <tr>
           <%= for pid <- @pids do %>
-            <th>PID <%= pid %></th>
+            <th class="tcol">PID <%= pid %></th>
           <% end %>
         </tr>
 
-        <!-- FFT: are we iterating by row idx or record? -->
         <%= for idx <- 0..@row_info.num_rows * 2 do %>
           <tr>
             <%= for pid <- @pids do %>
               <%= case row_content(@table_matrix, pid, idx) do %>
                 <% {:start, cell_info} -> %>
-                  <td rowspan="<%= cell_info.fill_size %>" style="background-color: yellow">
+                  <td class="tcol" rowspan="<%= cell_info.fill_size %>" style="background-color: yellow">
                     <%= Map.get(@records, cell_info.record_id).command %>
                   </td>
                 <% :fill -> %>
